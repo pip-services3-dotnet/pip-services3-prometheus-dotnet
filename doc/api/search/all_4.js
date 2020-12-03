6021f81d7c80c78474b04fb13678d4e8c330364c
banalna <judas.priest999@gmail.com>
2020-12-03 14:09:38 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['openasync',['OpenAsync',['../class_pip_services_1_1_prometheus_1_1_count_1_1_prometheus_counters.html#ac91b420a63a4fcfe7c1dab8758c48b56',1,'PipServices3::Prometheus::Count::PrometheusCounters']]]
-];
--- doc/api/search/namespaces_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['build',['Build',['../namespace_pip_services_1_1_prometheus_1_1_build.html',1,'PipServices3::Prometheus']]],
-  ['count',['Count',['../namespace_pip_services_1_1_prometheus_1_1_count.html',1,'PipServices3::Prometheus']]],
-  ['pipservices',['PipServices3',['../namespace_pip_services.html',1,'']]],
-  ['prometheus',['Prometheus',['../namespace_pip_services_1_1_prometheus.html',1,'PipServices3']]],
-  ['services',['Services',['../namespace_pip_services_1_1_prometheus_1_1_services.html',1,'PipServices3::Prometheus']]]
-];
